bd836cfc8bb8b238c772134fb630391d4790ef3a
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-01-26 22:26:00 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,9 @@ setup(
     name="json_wrapper",
     description="Easy JSON wrapper packed with features",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/RGBCube/json_wrapper",
-    version="1.0",
+    version="1.0.1",
     author="RGBCube",
     py_modules=["json_wrapper"],
     license="CC0 1.0 Universal"
