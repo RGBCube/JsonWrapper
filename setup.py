979170d304674ef284a29aaaba18b453844b0c0f
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-01-26 22:18:52 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
+from pathlib import Path
 
-readme = ""
-with open("README.md") as file:
-    readme = file.read()
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="json_wrapper",
     description="Easy JSON wrapper packed with features",
+    long_description=long_description,
     url="https://github.com/RGBCube/json_wrapper",
     version="1.0",
     author="RGBCube",
