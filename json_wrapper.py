9cee07f702695491d7dfae118362293fffabc8c3
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-01-27 18:59:45 +0300

    Update json_wrapper.py

--- json_wrapper.py
+++ json_wrapper.py
@@ -33,7 +33,7 @@ class _JsonUtils:
             with open(self.json_path, mode="r") as json_file:
                 data = json.load(json_file)
 
-        except (FileNotFoundError, ValueError):
+        except (FileNotFoundError, JSONDecodeError):
             with open(self.json_path, mode="w") as json_file:
                 json.dump({}, json_file)
                 return
