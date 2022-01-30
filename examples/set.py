c5a2c5de55efced375a7a6ceb9edc1eadea33371
RGBCube <78925721+RGBCube@users.noreply.github.com>
2022-01-30 11:44:34 +0300

    Update set.py

--- examples/set.py
+++ examples/set.py
@@ -1,5 +1,23 @@
-from json_wrapper import JsonWrapper
+from json_wrapper import JsonWrapper  # Initialization
 
-db = JsonWrapper("example.json")
+db = JsonWrapper("example.json")  # Initialization
 
-db.set("a", ["valu
+
+# set normal
+db.set("a", 123)
+
+data = db.all()
+
+print(data)  # {'a': 123}
+
+db.nuke()
+
+
+# set pathmagic
+db.set("a", 123, pathmagic="example+path")
+
+data = db.all()
+
+print(data)  # 'example': {'path': {'a': 123}}}
+
+db.nuke()
